296bb7259ebd5f9cc547cc92faa6b1af2f5174cc
ÆtherSurfer <aethersurfer@aethercraft.com>
2013-07-14 17:17:22 -0400

    color change with position test

--- src/main/scala/com/aethercraft/factionsmap/Renderer.scala
+++ src/main/scala/com/aethercraft/factionsmap/Renderer.scala
@@ -4,13 +4,14 @@ import org.bukkit.map.{MapPalette, MapCanvas, MapView, MapRenderer}
 import org.bukkit.entity.Player
 
 object Renderer extends MapRenderer(true) {
+  val ps = List(
+    (0,0),  (0,1),  (0,2),
+    (1,0),/*(1,1),*/(1,2),
+    (2,0),  (2,1),  (2,2)
+  )
   def render(map: MapView, canvas: MapCanvas, player: Player) {
-    val ps = List(
-      (0,0),  (0,1),  (0,2),
-      (1,0),/*(1,1),*/(1,2),
-      (2,0),  (2,1),  (2,2)
-    )
+    val c: Byte = (player.getLocation.getBlockX % 12 * 4).toByte
     for ((x,y) <- ps)
-      canvas.setPixel(x, y, MapPalette.RED)
+      canvas.setPixel(x, y, c)
   }
 }
