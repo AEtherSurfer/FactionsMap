f410a44f6c8088b0432734264eb9768a9ce7359d
ÆtherSurfer <aethersurfer@aethercraft.com>
2013-07-14 17:07:01 -0400

    clean up whitespace unused imports

--- src/main/scala/com/aethercraft/factionsmap/Plugin.scala
+++ src/main/scala/com/aethercraft/factionsmap/Plugin.scala
@@ -1,13 +1,9 @@
 package com.aethercraft.factionsmap
 
 import org.bukkit.plugin.java.JavaPlugin
-import org.bukkit.map.MapView
 
 class Plugin extends JavaPlugin {
-
   override def onEnable() {
     getServer.getPluginManager.registerEvents(new Listener(this), this)
   }
-  
-
 }
